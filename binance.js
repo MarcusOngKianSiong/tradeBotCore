42d46444766fe32a130e148876feb35147100222
marcus <marcusongkiansiong@gmail.com>
2023-03-11 02:54:49 +0800

    set up the necessary details such as api key and secret to interact with binance

---
+++ binance.js
@@ -0,0 +1,13 @@
+import crypto from 'crypto';
+import fetch from 'node-fetch'
+
+// const crypto = require('crypto')
+// const fetch = require('node-fetch')
+const api_key = '3aceb9dfacc4f0ae2dca00f1d2eec8ad7567a491372cd0f2e2d4c2734d720f59'
+const api_secret = 'd5b9b2e9d980da320b306676dd8afe3635e706110a5fa96a0e096ede3ff0c235'
+const baseURL = 'https://testnet.binancefuture.com'
+const headerAPIKey = {
+    'X-MBX-APIKEY': ''
+}
+
+// transactionHistory()
