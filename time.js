34e6a6e674151baf45ceb8d2eef1db15c9560319
marcus <marcusongkiansiong@gmail.com>
2023-03-21 01:47:57 +0800

    Added a new function called convertDateFromPSQLIntoSingaporeDate to handle the data format when date is being retrieved from order_history

--- time.js
+++ time.js
@@ -1,20 +1,32 @@
 // example timestamp: 1679320826630
 
-export function convertBinanceTimestampToDate(timestamp){
+
+export function convertBinanceTimestampToDateAndTime(timestamp){
     const dateAndTime = new Date(timestamp);
     dateAndTime.setHours(dateAndTime.getHours()+8);
     const singaporeDateAndTime = dateAndTime.toISOString();
     const data = singaporeDateAndTime.split("T")
-    return data[0]
+    data[1] = data[1].replace('Z','')
+    return data
 }
 
-export function convertBinanceTimestampToTime(timestamp){
-    const dateAndTime = new Date(timestamp);
-    dateAndTime.setHours(dateAndTime.getHours()+8);
-    const singaporeDateAndTime = dateAndTime.toISOString();
-    const data = singaporeDateAndTime.split("T");
-    return data[1].replace('Z','');
+// what is returned from psql is in zulu time format
+export function convertDateFromPSQLIntoSingaporeDate(date){
+    date.setHours(date.getHours()+8)
+    const singaporeDate = date.toISOString();
+    return singaporeDate.split("T")[0]
+    
 }
 
+// export function convertBinanceTimestampToTime(timestamp){
+//     const dateAndTime = new Date(timestamp);
+//     dateAndTime.setHours(dateAndTime.getHours()+8);
+//     const singaporeDateAndTime = dateAndTime.toISOString();
+//     const data = singaporeDateAndTime.split("T");
+//     return ;
+// }
+
+
+console.log(convertBinanceTimestampToDateAndTime(1679320826630))
 // convertBinanceTimestampToDate(1679320826630);
 // console.log(convertBinanceTimestampToTime(1679320826630))
